cf06bfe6cfb918d06911712a732156f4818cf611
sabeesh90 <48343095+sabeesh90@users.noreply.github.com>
2021-07-06 00:47:49 +0530

    Update main.py

--- main and utils/main.py
+++ main and utils/main.py
@@ -121,7 +121,7 @@ def test(model, device, testloader):
     test_loss = 0
     correct = 0
     with torch.no_grad():
-        for data, target in test_loader:
+        for data, target in testloader:
             data, target = data.to(device), target.to(device)
             output = model(data)
             test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
@@ -132,10 +132,10 @@ def test(model, device, testloader):
     test_losses.append(test_loss)
 
     print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
-        test_loss, correct, len(test_loader.dataset),
-        100. * correct / len(test_loader.dataset)))
+        test_loss, correct, len(testloader.dataset),
+        100. * correct / len(testloader.dataset)))
     
-    test_acc.append(100. * correct / len(test_loader.dataset))
+    test_acc.append(100. * correct / len(testloader.dataset))
 
 # def test(epoch):
 #     global best_acc

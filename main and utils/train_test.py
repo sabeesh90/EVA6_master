75d9ead8ba8a361c1279add0d81d6e25f52b6788
sabeesh90 <48343095+sabeesh90@users.noreply.github.com>
2021-07-06 02:36:34 +0530

    Update train_test.py

--- main and utils/train_test.py
+++ main and utils/train_test.py
@@ -1,5 +1,3 @@
-
-import torch.optim as optim
 ''' Train CIFAR10 with PyTorch.'''
 import torch
 import torch.nn as nn
@@ -16,62 +14,41 @@ from torch.optim.lr_scheduler import OneCycleLR
 from tqdm import tqdm
 
 
-train_losses = []
-train_acc = []
-test_losses = []
-test_acc = []
+def generate_dataset():
+    transform_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+    trainset = torchvision.datasets.CIFAR10(
+        root='./data', train=True, download=True, transform=transform_train)
+    trainloader = torch.utils.data.DataLoader(
+        trainset, batch_size=128, shuffle=True, num_workers=2)
+
+    testset = torchvision.datasets.CIFAR10(
+        root='./data', train=False, download=True, transform=transform_test)
+    testloader = torch.utils.data.DataLoader(
+        testset, batch_size=100, shuffle=False, num_workers=2)
+
+    classes = ('plane', 'car', 'bird', 'cat', 'deer',
+               'dog', 'frog', 'horse', 'ship', 'truck')
+
+    return trainloader, testloader
 
 
-def create_optim(model, epochs, trainloader):
-    optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
-    scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr=0.5, steps_per_epoch=len(trainloader), epochs=epochs)
-    return optimizer, scheduler
+def generate_model(model, input_size = (3,32,32)):
+    cuda = torch.cuda.is_available()
+    torch.manual_seed(1)
+    if cuda:
+        torch.cuda.manual_seed(1)
+    device = torch.device("cuda" if cuda else "cpu")
+    model_generated = model.to(device)
+    print(summary(model_generated, input_size=input_size))
+    return model_generated
 
-
-def train_model(epochs, model, trainloader, testloader, optimizer,scheduler,device='cuda'):
-    def train(model, device, trainloader, optimizer, epochs):
-    model.train()
-    print(len(trainloader))
-    pbar = tqdm(trainloader)
-    correct = 0
-    processed = 0
-    for batch_idx, (data, target) in enumerate(pbar):
-        data, target = data.to(device), target.to(device)
-        optimizer.zero_grad()
-        y_pred = model(data)
-        loss = F.cross_entropy(y_pred, target)
-        train_losses.append(loss)
-        loss.backward()
-        optimizer.step()
-        pred = y_pred.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
-        correct += pred.eq(target.view_as(pred)).sum().item()
-        processed += len(data)
-        pbar.set_description(desc= f'Loss={loss.item()} Batch_id={batch_idx} Accuracy={100*correct/processed:0.2f}')
-        train_acc.append(100*correct/processed)
-
-    def test(model, device, testloader):    
-        model.eval()
-        test_loss = 0
-        correct = 0
-        with torch.no_grad():
-            for data, target in testloader:
-                data, target = data.to(device), target.to(device)
-                output = model(data)
-                test_loss += F.cross_entropy(output, target, reduction='sum').item()  # sum up batch loss
-                pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
-                correct += pred.eq(target.view_as(pred)).sum().item()
-
-        test_loss /= len(testloader.dataset)
-        test_losses.append(test_loss)
-
-        print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
-            test_loss, correct, len(testloader.dataset),
-            100. * correct / len(testloader.dataset)))
-        
-        test_acc.append(100. * correct / len(testloader.dataset))
-    
-    for epoch in range(epochs):
-        print("EPOCH:", epoch)
-        train(model, device, trainloader, optimizer, epochs)
-        scheduler.step()
-        test(model, device, testloader)

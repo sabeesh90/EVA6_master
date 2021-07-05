ce0a4915e42868808719007586c08576b2e34af5
sabeesh90 <48343095+sabeesh90@users.noreply.github.com>
2021-07-06 01:53:06 +0530

    Add files via upload

---
+++ main and utils/main2.py
@@ -0,0 +1,72 @@
+''' Train CIFAR10 with PyTorch.'''
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.backends.cudnn as cudnn
+
+import torchvision
+import torchvision.transforms as transforms
+import os
+import argparse
+from models import *
+from utils import progress_bar
+from tqdm import tqdm
+from torchsummary import summary
+from torch.optim.lr_scheduler import OneCycleLR
+from tqdm import tqdm
+
+
+def generate_dataset():
+    transform_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+    trainset = torchvision.datasets.CIFAR10(
+        root='./data', train=True, download=True, transform=transform_train)
+    trainloader = torch.utils.data.DataLoader(
+        trainset, batch_size=128, shuffle=True, num_workers=2)
+
+    testset = torchvision.datasets.CIFAR10(
+        root='./data', train=False, download=True, transform=transform_test)
+    testloader = torch.utils.data.DataLoader(
+        testset, batch_size=100, shuffle=False, num_workers=2)
+
+    classes = ('plane', 'car', 'bird', 'cat', 'deer',
+               'dog', 'frog', 'horse', 'ship', 'truck')
+
+    return trainloader, testloader
+
+    
+def load_cuda():
+# Loading cuda
+    SEED = 1
+    cuda = torch.cuda.is_available()
+    print("CUDA Available?", cuda)
+    torch.manual_seed(SEED)
+    if cuda:
+        torch.cuda.manual_seed(SEED)
+    device = torch.device("cuda" if cuda else "cpu")
+    print(device)
+    return device
+
+def generate_model(model, input_size = (3,32,32)):
+    model_generated = model.to(device)
+    print(summary(model_generated, input_size=input_size))
+    return model_generated
+
+
+# defining the hyperparameters, loss functions and optimizers
+
+
+# for epoch in range(start_epoch, start_epoch+40):
+#     train(epoch)
+#     test(epoch)
+#     scheduler.step()

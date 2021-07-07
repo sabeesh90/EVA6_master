700c73b93e5345117944e25c631c76a211399569
sabeesh90 <48343095+sabeesh90@users.noreply.github.com>
2021-07-08 02:48:40 +0530

    Update main2.py

--- main and utils/main2.py
+++ main and utils/main2.py
@@ -56,9 +56,10 @@ def generate_dataset():
 
     train = Cifar10SearchDataset( train=True, download=True, transform=transform_train)
     test = Cifar10SearchDataset( train=False, download=True, transform=transform_test)
-    dataloader_args = dict(shuffle=True, batch_size=batch_size, num_workers=4, pin_memory=True)
-    trainloader = torch.utils.data.DataLoader(train, **dataloader_args)
-    testloader = torch.utils.data.DataLoader(test, **dataloader_args)
+    dataloader_args_train = dict(shuffle=True, batch_size=128, num_workers=4, pin_memory=True)
+    dataloader_args_test =dict(shuffle=True, batch_size=1, num_workers=4, pin_memory=True)
+    trainloader = torch.utils.data.DataLoader(train, **dataloader_args_test)
+    testloader = torch.utils.data.DataLoader(test, **dataloader_args_train)
 
     classes = ('plane', 'car', 'bird', 'cat', 'deer',
                'dog', 'frog', 'horse', 'ship', 'truck')

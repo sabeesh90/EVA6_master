2bb002165c5b2baeabb609ae9907821dcb408a98
sabeesh90 <48343095+sabeesh90@users.noreply.github.com>
2021-07-08 02:18:44 +0530

    Update main2.py

--- main and utils/main2.py
+++ main and utils/main2.py
@@ -12,20 +12,23 @@ from tqdm import tqdm
 from torchsummary import summary
 from torch.optim.lr_scheduler import OneCycleLR
 from tqdm import tqdm
+import albumentations as A
 
 
 def generate_dataset():
-    transform_train = transforms.Compose([
-        transforms.RandomCrop(32, padding=4),
-        transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    transform_train = A.Compose([
+        A.Rotate(+5,-5),
+        A.RandomCrop(32,32,p=0.1),
+        A.CoarseDropout(max_holes = 1, max_height=16, max_width=16, min_holes = 1, min_height=4, min_width=4, fill_value=channel_means), 
+        A.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        ToTensor(),
     ])
 
-    transform_test = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    transform_test = A.Compose([
+        A.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        ToTensor(),
     ])
+    
     trainset = torchvision.datasets.CIFAR10(
         root='./data', train=True, download=True, transform=transform_train)
     trainloader = torch.utils.data.DataLoader(

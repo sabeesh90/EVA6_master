3040bcaa3c2e8d7cd6a0fedc40b5f0f0493ea049
sabeesh90 <48343095+sabeesh90@users.noreply.github.com>
2021-07-12 22:27:09 +0530

    Update show_cam_bharath_manu_dinesh_sab.py

--- main and utils/show_cam_bharath_manu_dinesh_sab.py
+++ main and utils/show_cam_bharath_manu_dinesh_sab.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import cv2
 import torch 
 
-def generate_gradcam(predicted_class,pred,model, img, true_class_label, predicted_class_label):
+def generate_gradcam(predicted_class,pred,model, img, true_class_label, predicted_class_label, heat_gradient):
     pred[:,predicted_class].backward()
     gradients = model.get_activations_gradient()
     pooled_gradients = torch.mean(gradients, dim=[0, 2, 3])
@@ -20,9 +20,9 @@ def generate_gradcam(predicted_class,pred,model, img, true_class_label, predicte
     heatmap1 = cv2.resize(np.array(heatmap), (32, 32))
     heatmap1 = np.uint8(255 * heatmap1)
     heatmap1 = cv2.applyColorMap(heatmap1, cv2.COLORMAP_JET)
-    superimposed_img = heatmap1*0.005  + np.transpose(img[0].cpu().numpy(),(1,2,0))
+    superimposed_img = heatmap1*heat_gradient  + np.transpose(img[0].cpu().numpy(),(1,2,0))
     a = f'Original_class {true_class_label} predicted_class {predicted_class_label}'
-    plt.figure(figsize=(3,2))
+    plt.figure(figsize=(5,5))
     
     plt.subplot(1,2,1)
     plt.imshow(np.transpose(img[0].cpu().numpy(),(1,2,0))/2+0.5)
@@ -30,7 +30,7 @@ def generate_gradcam(predicted_class,pred,model, img, true_class_label, predicte
     plt.imshow(superimposed_img)
     plt.title(a)
     
-def get_true_pred(testloader,activated_mod,count_images,class_dictionary):
+def get_true_pred(testloader,activated_mod,count_images,class_dictionary, heat_gradient):
     passed = 0
     fail = 0
     count = 0
@@ -50,7 +50,7 @@ def get_true_pred(testloader,activated_mod,count_images,class_dictionary):
             true_class_label = class_dictionary[int(label.item())]
             predicted_class_label = class_dictionary[int(predicted_class)]
             # print(f'true_class={label.item()}',f'predicted_class={predicted_class}', False)
-            generate_gradcam(predicted_class, pred,activated_mod, img, true_class_label, predicted_class_label)
+            generate_gradcam(predicted_class, pred,activated_mod, img, true_class_label, predicted_class_label,heat_gradient)
             fail+=1
             if count >= count_images:
                 break

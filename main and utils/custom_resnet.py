2058ecd5e863a65e595a46d07b69c2598c6a3b39
sabeesh90 <48343095+sabeesh90@users.noreply.github.com>
2021-07-16 01:21:59 +0530

    Update custom_resnet.py

--- main and utils/custom_resnet.py
+++ main and utils/custom_resnet.py
@@ -2,19 +2,19 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-
 class BasicBlock(nn.Module):
     expansion = 1
 
-    def __init__(self, in_planes, planes, stride=1):
+    def __init__(self, in_planes, planes, count, stride=1):
         super(BasicBlock, self).__init__()
         self.conv1 = nn.Conv2d(
             in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(planes)
-       
-        # lets say stride  = 2  / inplanes  = 6 , expansion(2) * 6
         self.shortcut = nn.Sequential()
-        if stride != 1 or in_planes != self.expansion*planes:
+        self.count1= count
+
+        # if stride != 1 or in_planes != self.expansion*planes:
+        if self.count1 == 'a':
             self.shortcut = nn.Sequential(
                 nn.Conv2d(in_planes, self.expansion*planes,
                           kernel_size=1, stride=stride, bias=False),
@@ -27,8 +27,13 @@ class BasicBlock(nn.Module):
 
     def forward(self, x):
         out = F.relu(self.bn1(self.conv1(x)))
-        out += self.shortcut(x)
-        out = F.relu(out)
+        if self.count1 == 'a':
+            y  = self.shortcut(x)
+            print(out.shape, y.shape)   
+            out = out+y
+            out = F.relu(out)
+            return out
+        out = F.max_pool2d(out,kernel_size=(2,2))                 
         return out
 
 class ResNet(nn.Module):
@@ -39,16 +44,17 @@ class ResNet(nn.Module):
         self.conv1 = nn.Conv2d(3, 64, kernel_size=3,
                                stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(64)
-        self.layer1 = self._make_layer(block, 128, num_blocks[0], stride=1)
-        self.layer2 = self._make_layer(block, 256, num_blocks[1], stride=2)
-        self.layer3 = self._make_layer(block, 512, num_blocks[2], stride=2)
+        self.layer1 = self._make_layer(block, 128, num_blocks[0], count = 'a',stride=1 )
+        self.layer2 = self._make_layer(block, 256, num_blocks[1], count = 'b', stride=2)
+        self.layer3 = self._make_layer(block, 512, num_blocks[2], count = 'a', stride=1)
+        
         self.linear = nn.Linear(512*block.expansion, num_classes)
 
-    def _make_layer(self, block, planes, num_blocks, stride):
+    def _make_layer(self, block, planes, num_blocks, count,stride):
         strides = [stride] + [1]*(num_blocks-1)
         layers = []
         for stride in strides:
-            layers.append(block(self.in_planes, planes, stride))
+            layers.append(block(self.in_planes, planes, count, stride))
             self.in_planes = planes * block.expansion
         return nn.Sequential(*layers)
 

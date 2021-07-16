a255f5d4544471894e97573cac7ee6156427aa99
sabeesh90 <48343095+sabeesh90@users.noreply.github.com>
2021-07-16 16:39:28 +0530

    Add files via upload

---
+++ main and utils/train_test_normal.py
@@ -0,0 +1,70 @@
+
+import torch.optim as optim
+''' Train CIFAR10 with PyTorch.'''
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.backends.cudnn as cudnn
+import torchvision
+import torchvision.transforms as transforms
+from models import *
+from utils import progress_bar
+from tqdm import tqdm
+from torchsummary import summary
+from torch.optim.lr_scheduler import OneCycleLR
+from tqdm import tqdm
+
+
+train_losses = []
+train_acc = []
+test_losses = []
+test_acc = []
+mean_training_loss = []
+step_loss = 0
+
+def create_optim(model, trainloader, epochs=40):
+    optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
+    scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr=0.5, steps_per_epoch=len(trainloader), epochs=epochs)
+    return optimizer, scheduler
+
+
+def train(model, device, train_loader, optimizer, epoch):
+  model.train()
+  pbar = tqdm(train_loader)
+  correct = 0
+  processed = 0
+  for batch_idx, (data, target) in enumerate(pbar):
+    data, target = data.to(device), target.to(device)
+    optimizer.zero_grad()
+    y_pred = model(data)
+    loss = F.nll_loss(y_pred, target)
+    train_losses.append(loss)
+    loss.backward()
+    optimizer.step()
+    pred = y_pred.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+    correct += pred.eq(target.view_as(pred)).sum().item()
+    processed += len(data)
+    pbar.set_description(desc= f'Loss={loss.item()} Batch_id={batch_idx} Accuracy={100*correct/processed:0.2f}')
+    train_acc.append(100*correct/processed)
+
+def test(model, device, test_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
+            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+    test_losses.append(test_loss)
+
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+    
+    test_acc.append(100. * correct / len(test_loader.dataset))
